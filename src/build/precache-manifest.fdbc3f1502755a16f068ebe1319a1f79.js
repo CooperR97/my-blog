e40bc95278028ddd950b268df43b9a41dee5044e
co008202 <Cooper.Redfern@maxar.com>
2020-07-29 11:50:19 -0600

    Initial commit

---
+++ src/build/precache-manifest.fdbc3f1502755a16f068ebe1319a1f79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1579d5c8e5c39cc804a7923d0d66c5ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25378ed0466b3c977eeb",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "be31126ed522b9b5a923",
+    "url": "/static/js/2.5cd47a59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5cd47a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25378ed0466b3c977eeb",
+    "url": "/static/js/main.da599ad7.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
